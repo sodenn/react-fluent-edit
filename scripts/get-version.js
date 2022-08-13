78233018683f2166deaf61011ee6553350839274
sodenn <mail@sodenn.dev>
2022-08-13 13:15:05 +0200

    chore: pass branch name as argument

--- scripts/get-version.js
+++ scripts/get-version.js
@@ -2,15 +2,24 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
 async function getVersion() {
+  const args = process.argv.slice(2);
+  let branchName = args.length > 0 ? args[0] : undefined;
+
   const { stdout: latestVersion } = await exec(
     "npm show @react-fluent-edit/core version"
   );
 
   const { stdout: currentBranch } = await exec("git branch --show-current");
+  branchName = branchName || currentBranch;
+
+  if (!branchName) {
+    throw new Error("branchName is undefined");
+  }
+
   const tag =
-    currentBranch.trim() === "main"
+    branchName.trim() === "main"
       ? "next"
-      : currentBranch.trim().replaceAll(/[^a-zA-Z0-9]/g, "-");
+      : branchName.trim().replaceAll(/[^a-zA-Z0-9]/g, "-");
 
   const version = `${latestVersion.trim()}-${tag}`;
 
