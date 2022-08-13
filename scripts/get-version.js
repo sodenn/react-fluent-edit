995fbf52c0658f40072e1f42d3d27038f6a6e70a
Dennis Soehnen <mail@sodenn.dev>
2022-08-13 20:06:55 +0200

    chore: improvements to the release process (#28)

--- scripts/get-version.js
+++ scripts/get-version.js
@@ -1,13 +1,11 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
+const corePkg = "@react-fluent-edit/core";
+
 async function getVersion() {
   const args = process.argv.slice(2);
   let branchName = args.length > 0 ? args[0] : undefined;
-
-  const { stdout: latestVersion } = await exec(
-    "npm show @react-fluent-edit/core version"
-  );
 
   const { stdout: currentBranch } = await exec("git branch --show-current");
   branchName = branchName || currentBranch;
@@ -21,26 +19,31 @@ async function getVersion() {
       ? "next"
       : branchName.trim().replaceAll(/[^a-zA-Z0-9]/g, "-");
 
+  const { stdout: latestVersion } = await exec(`npm show ${corePkg} version`);
+
   const version = `${latestVersion.trim()}-${tag}`;
 
   let count = 0;
-  const { stdout: prevVersionsStr } = await exec(
-    `npm show @react-fluent-edit/core@${version} version --json`
+  const { stdout: allVersions } = await exec(
+    `npm show ${corePkg} versions --json`
   );
   try {
-    let prevVersions = JSON.parse(prevVersionsStr);
-    prevVersions = prevVersions.sort((a, b) => b.localeCompare(a));
-    const prevVersion = prevVersions[0];
-    const prevCount = prevVersion.substring(prevVersion.lastIndexOf(".") + 1);
-    count = parseInt(prevCount) + 1;
+    const prevVersions = JSON.parse(allVersions)
+      .sort((a, b) => b.localeCompare(a))
+      .filter((v) => v.startsWith(version));
+    if (prevVersions.length > 0) {
+      const prevVersion = prevVersions[0];
+      const prevCount = prevVersion.substring(prevVersion.lastIndexOf(".") + 1);
+      count = parseInt(prevCount) + 1;
+    }
   } catch (e) {
     //
   }
 
-  const betaVersion = `${version}.${count}`;
+  const nextVersion = `${version}.${count}`;
 
-  console.log(betaVersion);
-  return betaVersion;
+  console.log(nextVersion);
+  return nextVersion;
 }
 
 getVersion();
