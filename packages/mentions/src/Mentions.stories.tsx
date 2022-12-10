be8fdfe3a1b0a53a064e21148ee4527e8fff350a
Dennis Soehnen <mail@sodenn.dev>
2022-12-10 14:48:10 +0100

    chore(deps): update dependency storybook to v7  (#93)

--- packages/mentions/src/Mentions.stories.tsx
+++ packages/mentions/src/Mentions.stories.tsx
@@ -1,30 +1,27 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
 import MentionsCode from "./Mentions.code";
 import MentionsStory from "./Mentions.stage";
 
 export default {
   title: "FluentEdit",
   component: MentionsStory,
-} as ComponentMeta<typeof MentionsStory>;
-
-const Template: ComponentStory<typeof MentionsStory> = (args) => (
-  <MentionsStory {...args} />
-);
-
-export const Mentions = Template.bind({});
-Mentions.args = {
-  initialValue: "Hello @John",
-  placeholder: "Start typing",
-  autoCorrect: "off",
-  autoCapitalize: "off",
-  spellCheck: false,
-  autoFocus: true,
-  singleLine: false,
 };
 
-Mentions.parameters = {
-  docs: {
-    source: { code: MentionsCode },
-    language: "typescript",
+export const Mentions = {
+  args: {
+    initialValue: "Hello @John",
+    placeholder: "Start typing",
+    autoCorrect: "off",
+    autoCapitalize: "off",
+    spellCheck: false,
+    autoFocus: true,
+    singleLine: false,
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: MentionsCode,
+        language: "typescript",
+      },
+    },
   },
 };
