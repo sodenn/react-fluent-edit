72608f150a0bfe0bc459e1b0a024e420fad7b51a
sodenn <mail@sodenn.dev>
2022-12-10 13:02:19 +0100

    remove usage of deprecated sb interfaces

--- packages/markdown/src/Markdown.stories.tsx
+++ packages/markdown/src/Markdown.stories.tsx
@@ -1,31 +1,28 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
 import MarkdownCode from "./Markdown.code";
 import MarkdownStory from "./Markdown.stage";
 
 export default {
   title: "FluentEdit",
   component: MarkdownStory,
-} as ComponentMeta<typeof MarkdownStory>;
-
-const Template: ComponentStory<typeof MarkdownStory> = (args) => (
-  <MarkdownStory {...args} />
-);
-
-export const Markdown = Template.bind({});
-Markdown.args = {
-  initialValue:
-    "# Heading\nLorem *ipsum **dolor** sit amet*\n\n1. Aaa\n   1. Bbb\n   2. Ccc\n   3. Ddd\n2. Eee",
-  placeholder: "Start typing",
-  autoCorrect: "off",
-  autoCapitalize: "off",
-  spellCheck: false,
-  autoFocus: true,
-  singleLine: false,
 };
 
-Markdown.parameters = {
-  docs: {
-    source: { code: MarkdownCode },
-    language: "typescript",
+export const Markdown = {
+  args: {
+    initialValue:
+      "# Heading\nLorem *ipsum **dolor** sit amet*\n\n1. Aaa\n   1. Bbb\n   2. Ccc\n   3. Ddd\n2. Eee",
+    placeholder: "Start typing",
+    autoCorrect: "off",
+    autoCapitalize: "off",
+    spellCheck: false,
+    autoFocus: true,
+    singleLine: false,
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: MarkdownCode,
+        language: "typescript",
+      },
+    },
   },
 };
